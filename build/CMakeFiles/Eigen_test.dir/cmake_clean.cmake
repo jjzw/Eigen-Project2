bb970072283380f8044828fb0ca07aa687be1d2e
Jianjie Lin <jianjie.lin@outlook.com>
2017-07-17 09:45:04 +0200

    Initial commit

---
+++ build/CMakeFiles/Eigen_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Eigen_test.dir/Eigen_test.cpp.o"
+  "Eigen_test.pdb"
+  "Eigen_test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Eigen_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
